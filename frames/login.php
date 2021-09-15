bdd3243dd7e54d972350727e351ef12efea4ccd0
DasNasu <motzkus.hauke@gmail.com>
2021-09-15 08:12:54 +0200

    fixed linking
    fixed the bug where the login on the production page linked to the development page and thus logged the user in on the development page.

--- frames/login.php
+++ frames/login.php
@@ -1,7 +1,7 @@
 <div class="loginarea">
 	<?php
 		if(isset($_SESSION['user']) && !empty($_SESSION['user'])) {
-			echo 'Willkommen '.$_SESSION['user'].' <a href="./?logout">Logout</a>';
+			echo 'Willkommen '.$_SESSION['user'].' <a href="../?logout">Logout</a>';
 		} else {
 			?>
 				<form action="./" method="POST">
@@ -15,20 +15,20 @@
 			$data = $dbmanager->getLogin($_POST['user']);
 			if($data['pass'] === hash('sha256', $_POST['pass']) && $data['active'] == 1) {
 				$_SESSION['user'] = $_POST['user'];
-				echo '<meta http-equiv="refresh" content="0; URL=https://dasnasu.bitbite.dev/Controlpanel">';
+				echo '<meta http-equiv="refresh" content="0; URL=https://fiaede.dasnasu.bitbite.dev/Controlpanel">';
 			} elseif(!$data) {
-				echo '<meta http-equiv="refresh" content="0; URL=https://dasnasu.bitbite.dev/Loginerror?c=notcreated">';
+				echo '<meta http-equiv="refresh" content="0; URL=https://fiaede.dasnasu.bitbite.dev/Loginerror?c=notcreated">';
 			} elseif($data['active'] != 1) {
-				echo '<meta http-equiv="refresh" content="0; URL=https://dasnasu.bitbite.dev/Loginerror?c=inactive">';
+				echo '<meta http-equiv="refresh" content="0; URL=https://fiaede.dasnasu.bitbite.dev/Loginerror?c=inactive">';
 			} else {
-				echo '<meta http-equiv="refresh" content="0; URL=https://dasnasu.bitbite.dev/Loginerror?c=misstype">';
+				echo '<meta http-equiv="refresh" content="0; URL=https://fiaede.dasnasu.bitbite.dev/Loginerror?c=misstype">';
 			}
 		} elseif(isset($_GET['autologout'])) {
 			session_destroy();
-			echo '<meta http-equiv="refresh" content="0; URL=https://dasnasu.bitbite.dev/Logout?c=auto">';
+			echo '<meta http-equiv="refresh" content="0; URL=https://fiaede.dasnasu.bitbite.dev/Logout?c=auto">';
 		} elseif(isset($_GET['logout'])) {
 			session_destroy();
-			echo '<meta http-equiv="refresh" content="0; URL=https://dasnasu.bitbite.dev/Logout">';
+			echo '<meta http-equiv="refresh" content="0; URL=https://fiaede.dasnasu.bitbite.dev/Logout">';
 		}
 	?>
 </div>
