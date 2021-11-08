f69a38b2a7db9c97c493d8dfb696530c80f62db4
DasNasu <motzkus.hauke@gmail.com>
2021-11-08 14:23:06 +0100

    Squashed commit of the following:
    commit 9a2091dcc02c0afb018cb74bfad6c83217366590
    Author: DasNasu <motzkus.hauke@gmail.com>
    Date:   Mon Nov 8 14:19:58 2021 +0100
    
        some styling and linkage added
    
    commit 3ea7565763f85e01f29f535da430152c425a606a
    Author: DasNasu <motzkus.hauke@gmail.com>
    Date:   Mon Nov 8 14:19:24 2021 +0100
    
        added fileupload functionality
    
    commit bcffd27c3e50af9d9065e0b19e1cdf437e800c00
    Author: DasNasu <motzkus.hauke@gmail.com>
    Date:   Mon Nov 8 14:18:46 2021 +0100
    
        Login check changes
    
    commit 3f89bd213ecbe44dcace395e3b491c8ea6c16b27
    Author: DasNasu <motzkus.hauke@gmail.com>
    Date:   Mon Oct 11 09:48:55 2021 +0200
    
        fixed security leak on loginerror, index rework
    
        changed the error handlin on loginerrors to not show if an useraccount is inactive since it would tell attackers that an user account with that username does exist.
        reworked the index to require login to display the page at all, due to legal issues in germany.
    
    commit 0d83a3a768705594b1391a212c6dcb7cffbcb587
    Author: DasNasu <motzkus.hauke@gmail.com>
    Date:   Tue Sep 28 13:02:36 2021 +0200
    
        Fixed Issue #9
    
        Changed the condition that checkes wether you are on Logout or some other page to decide where to forward to also recognize if you were on Loginerror.
    
    commit 46be56862371e7ff16edd6861aa4ace939f2bdae
    Author: DasNasu <motzkus.hauke@gmail.com>
    Date:   Tue Sep 28 12:39:16 2021 +0200
    
        Download Bufix, Issue #6
    
    commit 00d6e2e2d0295d370cfedd60675b2af6fe6926d8
    Author: DasNasu <motzkus.hauke@gmail.com>
    Date:   Mon Sep 27 10:14:14 2021 +0200
    
        cookie bugfix and introduction of codesnippets
    
        fixed a bug where setting the cookie from subpages doesn't set the theming correctly for the entire page.
        introduction of codesnippets
    
    commit 047863d82636cff9cec089be7979cecf1484e391
    Author: DasNasu <motzkus.hauke@gmail.com>
    Date:   Fri Sep 17 16:46:12 2021 +0200
    
        cleanup, changed file extensioons for static files
    
    commit fb159466f04442bbde41e74b0e02cb18b91e58de
    Author: DasNasu <motzkus.hauke@gmail.com>
    Date:   Fri Sep 17 15:56:49 2021 +0200
    
        database normalizing
    
    commit 7bab2db3210668b983fd8c5353051eb8b85cf52a
    Author: DasNasu <motzkus.hauke@gmail.com>
    Date:   Fri Sep 17 13:27:18 2021 +0200
    
        filesize formatting
    
        formatted the filesize shown on the page to be more readable and have units

--- index.php
+++ index.php
@@ -2,43 +2,40 @@
 <html lang="de">
 <?php
 	session_start();
-	if(isset($_SESSION['user']) && !empty($_SESSION['user'])) header("refresh:1200; url=/?autologout");
-	$cookieOptions = array('expires' => time()+3600*24*30, 'path' => '/', 'domain' => '.dasnasu.bitbite.dev');
-	if(!isset($_COOKIE['theme'])) setcookie('theme', 'light', $cookieOptions);
-	
-	//requirements
-	require('./backend/databaselogin.php');
-	require('./backend/dbmanager.php');
-	require('./frames/navigation.php');
+	// GLOBAL REQUIREMENTS
+	require_once('./backend/databaselogin.php');
+	require_once('./backend/dbmanager.php');
 
-
-	//global objects
+	// GLOBAL VARIABLES / OBJECTS
 	$dbmanager = new DBManager($host, $database, $login, $pass);
+	require_once('./frames/head.php');
+	if(sizeof($_SESSION) === 0) {
+		require('./frames/login.php');
+	} else {
+		if(isset($_SESSION['user']) && !empty($_SESSION['user'])) header("refresh:1200; url=/?autologout");
+		$cookieOptions = array('expires' => time()+3600*24*30, 'path' => '/', 'domain' => '.dasnasu.bitbite.dev');
+		if(!isset($_COOKIE['theme'])) setcookie('theme', 'light', $cookieOptions);
 
-	//page requirements
-	$navigation = new Navigation($dbmanager->getNavigationItems("navigation"));
-	$footer = new Navigation($dbmanager->getNavigationItems("footernav"));
+		require_once('./frames/navigation.php');
 
+		$navigation = new Navigation($dbmanager->getNavigationItems("navigation"));
+		$footer = new Navigation($dbmanager->getNavigationItems("footernav"));
 
-
-	//current
-	require('./frames/head.php');
-	?>
-	<div class="profileMenu">
-	<?php
-	require('./frames/login.php');
-	?>
-		<div class="themeselector">
-			<select name="themeselection" id="themeselection" onChange="themeselect();">
-				<option value="default" <?php if(!isset($_COOKIE['theme']) || $_COOKIE['theme'] == 'default') { echo 'selected'; } ?>>Default Theme (Switch on Day/Night)</option>
-				<option value="light" <?php if(isset($_COOKIE['theme']) && $_COOKIE['theme'] == 'light') { echo 'selected'; } ?>>Light Theme</option>
-				<option value="dark" <?php if(isset($_COOKIE['theme']) && $_COOKIE['theme'] == 'dark') { echo 'selected'; } ?>>Dark Theme</option>
-			</select>
-		</div>
-	</div>
-	<?php
-	require('./frames/header.php');
-	echo $navigation->getMainNavigation();
-	require('./frames/content.php');
-	echo $footer->getFooterNavigation();
+		echo '<div class="profileMenu">';
+			require('./frames/login.php'); ?>
+				<div class="themeselector">
+					<select name="themeselection" id="themeselection" onChange="themeselect();">
+						<option value="default" <?php if(!isset($_COOKIE['theme']) || $_COOKIE['theme'] == 'default') { echo 'selected'; } ?>>Default Theme (Switch on Day/Night)</option>
+						<option value="light" <?php if(isset($_COOKIE['theme']) && $_COOKIE['theme'] == 'light') { echo 'selected'; } ?>>Light Theme</option>
+						<option value="dark" <?php if(isset($_COOKIE['theme']) && $_COOKIE['theme'] == 'dark') { echo 'selected'; } ?>>Dark Theme</option>
+					</select>
+				</div>
+			</div><?php
+		require_once('./frames/header.html');
+		
+		echo $navigation->getMainNavigation();
+		require('./frames/linkage.html');
+		require('./frames/content.php');
+		echo $footer->getFooterNavigation();
+	}
 ?>
---
+++ pages/Upload.php
@@ -0,0 +1,98 @@
+<script>
+    function toggleCategories() {
+        var select = document.getElementById("categories");
+        var resources = document.getElementById("resources");
+        if(select.value == "Ressourcen") {
+            document.getElementById("resources").style.display = "inline";
+            document.getElementById("materials").style.display = "none";
+            if(resources.value == "Buecher") {
+                document.getElementById('operatingsystems').style.display = "none";
+            } else {
+                document.getElementById('operatingsystems').style.display = "inline";
+            }
+        }
+        if(select.value == "Unterrichtsmaterialien") {
+            document.getElementById("materials").style.display = "inline";
+            document.getElementById("resources").style.display = "none";
+            document.getElementById('operatingsystems').style.display = "none";
+        }
+        
+    }
+</script>
+<style>
+    .toggleshow {
+        display: none;
+    }
+
+    #resources {
+        display: inline;
+    }
+</style>
+<div class="headline">
+    Upload
+</div>
+<article>
+    Depending on filsize and internet connection, fileuploads can take up some time. Please be patient and keep this site open until you got the response from the server. Feel free to use this site in a parallel tab.
+    <form action="send" method="post" enctype="multipart/form-data" onChange="toggleCategories();">
+        <select name="categories" id="categories">
+            <option value="Ressourcen">Ressourcen</option>
+            <option value="Unterrichtsmaterialien">Unterrichtsmaterialien</option>
+        </select>
+        <select name="resources" class="toggleshow" id="resources">
+            <option selected value="Buecher">B&uuml;cher</option>
+            <option value="Betriebssysteme">Betriebssysteme</option>
+        </select>
+        <select name="materials" class="toggleshow" id="materials">
+            <option selected value="Allgemein">Allgemein</option>
+            <option value="BGP-DS">BGP-DS</option>
+            <option value="BGP-HK">BGP-HK</option>
+            <option value="BGP-WiSo">BGP-WiSo</option>
+            <option value="EDV-LAT">EDV-LAT</option>
+            <option value="FaEng">FaEng</option>
+            <option value="KRE_TRE-LAT">KRE_TRE-LAT</option>
+            <option value="AnwP-Python">AnwP-Python</option>
+            <option value="BGP-QM">BGP-QM</option>
+            <option value="ITT-PC">ITT-PC</option>
+            <option value="LAT-LAT">LAT-LAT</option>
+            <option value="ITT-Net">ITT-Net</option>
+        </select>
+        <select name="operatingsystems" class="toggleshow" id="operatingsystems">
+            <option selected value="Arch Linux">Arch Linux</option>
+            <option value="CentOS">CentOS</option>
+            <option value="Debian">Debian</option>
+            <option value="DragonflyBSD">DragonflyBSD</option>
+            <option value="Fedora">Fedora</option>
+            <option value="FreeBSD">FreeBSD</option>
+            <option value="Gentoo">Gentoo</option>
+            <option value="Kali Linux">Kali Linux</option>
+            <option value="Linux Mint">Linux Mint</option>
+            <option value="Mac OS">Mac OS</option>
+            <option value="OpenBSD">OpenBSD</option>
+            <option value="OpenSuse">OpenSuse</option>
+            <option value="pFsense">pFsense</option>
+            <option value="TrueNAS">TrueNAS</option>
+            <option value="Ubuntu Desktop">Ubuntu Desktop</option>
+            <option value="Ubuntu Server">Ubuntu Server</option>
+            <option value="Windows 10">Windows 10</option>
+            <option value="Windows 11">Windows 11</option>
+            <option value="Windows 2000">Windows 2000</option>
+            <option value="Windows 7">Windows 7</option>
+            <option value="Windows 95">Windows 95</option>
+            <option value="Windows 98">Windows 98</option>
+            <option value="Windows ME">Windows ME</option>
+            <option value="Windows NT">Windows NT</option>
+            <option value="Windows XP">Windows XP</option>
+        </select>
+        <input type="file" name="file" style="min-width: 600px"/>
+        <input type="submit" value="Upload" />
+    </form>
+    <?php
+        if(isset($_POST['category'])) {
+            echo $_POST['category'].'<br />';
+            echo $_POST['resources'].'<br />';
+            echo $_POST['materials'].'<br />';
+            echo $_POST['operatingsystems'].'<br />';
+            var_dump($_FILES['file']);
+        }
+    ?>
+</article>
