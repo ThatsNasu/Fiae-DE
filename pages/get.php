3916b03662f5c409a939c2084b6c0c12cf4658cb
DasNasu <motzkus.hauke@gmail.com>
2021-11-09 18:18:21 +0100

    download/upload error fix
    Fixed an error where large files ( > 2,5gb) caused an integer overflow displaying the wrong filesize in download dialogue and interrupting the download after a certain amount of time (30+2 sec) passed, due to server configuration. Almost the same applies to the upload functionality.

--- pages/get.php
+++ pages/get.php
@@ -1,19 +1,19 @@
 <?php
+    ini_set('max_execution_time', '-1');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/backend/databaselogin.php');
     require_once($_SERVER['DOCUMENT_ROOT'].'/backend/dbmanager.php');
     $dbmanager = new DBManager($host, $database, $login, $pass);
     if(isset($_GET['f']) && !empty($_GET['f'])) {
         $file = $dbmanager->getFileByID($_GET['f']);
         if(file_exists($file['diskpath'].$file['filename'])) {
-            echo 'file exists';
             header('Content-Description: File Transfer');
             header('Content-Type: application/octet-stream');
             header('Content-Disposition: attachment; filename="'.$file['filename'].'"');
             header('Expires: 0');
-            header('Cache-Control: must-revalidate');
+            header('Cache-Control: no-cache');
             header('Pragma: public');
-            header('Content-Length: '.filesize($file['diskpath'].$file['filename']));
-            readfile($file['diskpath'].$file['filename']);
+            header('Content-Length: '.$file['size']);
+            fpassthru(fopen($file['diskpath'].$file['filename'], 'rb'));
             exit;
         }
         echo 'file doesnt exist';
